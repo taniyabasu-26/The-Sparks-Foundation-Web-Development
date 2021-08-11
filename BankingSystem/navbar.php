1effb03cdc10edbda6854a2c48403182e7e81cef
Taniya Basu <87356656+taniyabasu-26@users.noreply.github.com>
2021-08-11 15:12:31 +0530

    Update navbar.php

--- BankingSystem/navbar.php
+++ BankingSystem/navbar.php
@@ -1,6 +1,6 @@
 <!-- navbar --> 
       <nav class="navbar navbar-expand-md navbar-light bg-light">
-      <a class="navbar-brand" href="index.php">Welcome to Chirag Bank</a>
+      <a class="navbar-brand" href="index.php">Welcome to Taniya's Bank</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
         <span class="navbar-toggler-icon"></span>
       </button>
